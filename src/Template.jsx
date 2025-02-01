0a5bf193889932ba3745d4b81ca267feb369ec3a
RectorDev <Rector.dev@gmail.com>
2025-02-01 17:34:34 +0330

    download added

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Profile from './stacks/Profile'
+import { useRef } from "react";
+import Profile from "./stacks/Profile";
 import Bio from "./stacks/Bio.jsx";
 import Quote from "./stacks/Quote.jsx";
 import Personality from "./stacks/Personality.jsx";
@@ -10,19 +10,54 @@ import Payments from "./stacks/Payments.jsx";
 import Platform from "./stacks/Platform.jsx";
 import ThemeToggle from "./ThemeToggle.jsx";
 
+import html2canvas from "html2canvas";
+
 const Template = () => {
-    return (
-        <section className=' my-0 mx-auto max-w-5xl  bg-primary p-4 '>
-            <div className={'flex flex-1 gap-4'}>
-                <div className={'flex flex-col gap-4 flex-shrink-0 max-w-[26%]'}><Profile/><Quote/><Personality/></div>
-                <div className={'flex flex-col gap-4 flex-1'}><Bio/><CoreNeeds/><Frustrations/><Brands/>
-                    <div className={'flex-row flex gap-4 w-full'}><Payments/><Platform/></div>
-                </div>
-            </div>
+  const divRef = useRef(null);
 
-          <div className={'absolute left-0 top-0'}><ThemeToggle/></div>
-        </section>
-    )
-}
+  const handleDownload = () => {
+    if (divRef.current) {
+      // Capture the div content as an image
+      html2canvas(divRef.current).then((canvas) => {
+        // Convert the canvas to a data URL
+        const image = canvas.toDataURL("image/png");
 
-export default Template
+        // Create a temporary link element
+        const link = document.createElement("a");
+        link.href = image;
+        link.download = "div-content.png"; // File name
+        link.click(); // Trigger the download
+      });
+    }
+  };
+
+  return (
+    <section ref={divRef} className=" my-0 mx-auto max-w-5xl  bg-primary p-4 ">
+      <div className={"flex flex-1 gap-4"}>
+        <div className={"flex flex-col gap-4 flex-shrink-0 max-w-[26%]"}>
+          <Profile />
+          <Quote />
+          <Personality />
+        </div>
+        <div className={"flex flex-col gap-4 flex-1"}>
+          <Bio />
+          <CoreNeeds />
+          <Frustrations />
+          <Brands />
+          <div className={"flex-row flex gap-4 w-full"}>
+            <Payments />
+            <Platform />
+          </div>
+        </div>
+      </div>
+      <div className={"absolute left-0 top-0"}>
+        <ThemeToggle />
+      </div>{" "}
+      <button className={'absolute bg-slate-800 text-white px-4 text-lg border-2 rounded-lg border-black py-2 right-6 bottom-6'} onClick={handleDownload}>
+        download
+      </button>
+    </section>
+  );
+};
+
+export default Template;
